c1c869bf4c35ead3c043d206518a207a948abdc4
D.W.Ham <hebong79@gmail.com>
2023-07-21 09:20:25 +0900

    body-parser 사용

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
@@ -11,8 +12,12 @@ const home = require("./src/routes/home");
 //앱셋팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
 app.use(express.static(`${__dirname}/src/public`));  // __dirname -> app.js가 있는 현재디렉토리
+
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use("/", home);   // use -> 미들웨어를 등록하는 함수
 
 
