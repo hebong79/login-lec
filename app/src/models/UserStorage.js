5c7ccd752b74d091049725a817fa6fe89ce44af9
D.W.Ham <hebong79@gmail.com>
2023-07-26 10:13:35 +0900

    유저스토리지 제작

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,23 @@
+"use strict"
+// 변수에 #을 붙이면 private 변수로 변환된다.
+class UserStorage {
+    static #users = {
+        id : ["hdw", "김팀장", "공대장"],
+        password : ["1234", "1234", "12345"], 
+        name: ["hdw", "kim", "kong"],
+    };
+
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field)=>{
+            if( users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        //console.log(newUsers)
+        return newUsers;
+    }
+}
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const users = {
-    id : ["hdw", "김팀장", "공대장"],
-    password : ["1234", "1234", "12345"], 
-};
+const UserStorage = require("../../models/UserStorage");
 
 
 const output = {
@@ -20,19 +17,19 @@ const process = {
     login: (req, res) =>{
         const id = req.body.id,
            pass = req.body.password;
-
+        
+        const users = UserStorage.getUsers("id", "password");   
+        const response = {};
         if( users.id.includes(id) ){
             const idx = users.id.indexOf(id);
             if( users.password[idx] === pass) {
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
-        return res.json({
-            success : false,
-            msg : "로그인에 실패했습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패했습니다."; 
+        return res.json(response);
     },
 };
 
